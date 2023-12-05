b727ec325b1c855b7ae6c8cb46f67ffe42c04cad
Ishank Agrawal <agr.ishank@gmail.com>
2023-12-05 11:31:35 -0500

    disable cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const cors = require('cors');
 const path = require('path');
 const { spawn } = require("child_process");
 
@@ -14,6 +15,22 @@ const upload = multer({ storage });
 
 const app = express();
 const port = 8000;
+
+const allowedOrigins = ['https://mathletema.github.io/',];
+app.use(cors({
+  origin: function(origin, callback){
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (allowedOrigins.includes(origin)) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+
+}));
 
 const python_dir = "C:\\Users\\Ishank\\python-3.10\\"
 const fastnet_py = path.join(__dirname, "../fastnst-python/fastnst.py")
